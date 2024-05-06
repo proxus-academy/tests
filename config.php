04af4e6eab8e052aa98351a3a76e7aa0253d7bef
proxus-academy <147545848+proxus-academy@users.noreply.github.com>
2024-05-06 08:28:43 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php define('DB_NAME', 'flexam');
+    // Entorno de desarrollo local
+define('DB_HOST', 'localhost');
+define('DB_USER', 'flexam');
+define('DB_PASSWORD', 'Flexam24');
+define('BASE_URL', 'https://proxustools.com/');
+define('BASE_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
+define('COMMON_PATH', BASE_PATH . 'code/comun/');
+define('VIEWS_PATH',
+BASE_PATH . 'code/views/');
+// Definir otras rutas relativas a BASE_URL si es necesario
+define('STYLES_URL', BASE_URL . 'styles/');
+define('RESOURCES_URL', BASE_URL . 'resources/');
+?>
